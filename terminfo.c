972f5d263e0809079d439754e1b8548d938a5af4
Paul Hood <hoodp@mail.gvsu.edu>
2016-01-24 20:31:35 -0500

    modified flow

--- terminfo.c
+++ terminfo.c
@@ -2,27 +2,84 @@
 #include <term.h>
 #include <stdio.h>
 
-/*int setupterm(char *term, int fildes, int *errret);
-int del_curterm(TERMINAL *oterm);
-int restartterm(char *term, int fildes, int *errret);
-char *tparm(char *str, ...);
-int tputs(const char *str, int affcnt, int (*putc)(int));
-int putp(const char *str);
-int vidputs(chtype attrs, int (*putc)(int));
-int vidattr(chtype attrs);
-int vid_puts(attr_t attrs, short pair, void *opts, int (*putc)(char));
-int vid_attr(attr_t attrs, short pair, void *opts);
-int mvcur(int oldrow, int oldcol, int newrow, int newcol);
-int tigetflag(char *capname);
-int tigetnum(char *capname);
-char *tigetstr(char *capname);*/
+#define NUM_OPTIONS 4
+
+int getOption();
+int validOption(int option);
+void setup();
+int getSize();
+void displaySize();
+void clearScreen();
+
+int nrows, ncolumns;
+
+const char * options[NUM_OPTIONS] = {
+  "Display # of rows & columns",
+  "Clear the screen",
+  "Print to an arbitrary location",
+  "Enter data & print to an arbitrary location"
+};
 
 int main()
 {
-  int nrows, ncolumns;
+
+  // connect to lcurses library
+  setup();
+  
+  // holds input value 
+  int option = getOption();
+  int valid = validOption(option);
+  while(valid) {
+
+    // handle the option request
+    switch (option) {
+    case 1:
+      displaySize();
+      break;
+    case 2: 
+      clearScreen();
+      break;
+    default:
+      printf("error\n");
+    }
+    option = getOption();
+    valid = validOption(option);
+  }
+}
+
+void setup()
+{
   setupterm(NULL, fileno(stdout), (int *)0);
+}
+
+int getOption() 
+{
+  
+  // print each option
+  size_t i;
+  for (i = 0; i < sizeof(options) / sizeof(options[0]); i++) {
+    printf("(%lu): %s\n", i + 1, options[i]);
+  }
+  
+  // get input from the user & return the value 
+  int in;
+  scanf("%d", &in);
+  return in;
+}
+
+int validOption(int option)
+{
+  return option > 0 && option <= NUM_OPTIONS;
+}
+
+void displaySize()
+{
   nrows = tigetnum("lines");
   ncolumns = tigetnum("cols");
-  printf("This terminal has %d columns and %d rows.\n", ncolumns, nrows);
-  return 0;
+  printf("This screen has %d rows & %d columns.\n", nrows, ncolumns);
+}
+
+void clearScreen()
+{
+
 }
